ce59033be06a9ff2543c9438df9762475b696c86
maxey <emgireyes17@gmail.com>
2026-09-07 21:33:19 +0800

    let the generator validate the String length and let codeChallenge own both the verifier and the base64 encoded String

--- src/main/java/com/otfly/auth/OAuthHandler.java
+++ src/main/java/com/otfly/auth/OAuthHandler.java
@@ -3,8 +3,8 @@ package com.otfly.auth;
 import java.security.SecureRandom;
 import java.security.MessageDigest;
 import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 public final class OAuthHandler 
 {
@@ -14,6 +14,11 @@ public final class OAuthHandler
         final SecureRandom RANDOM = new SecureRandom();
         StringBuilder result = new StringBuilder(length);
         int i = 0;
+
+        if(length < 43 || length > 128)
+        {
+            throw new IllegalArgumentException("Verifier length must be between 43 and 128.");
+        }
 
         while(i < length)
         {
@@ -25,29 +30,38 @@ public final class OAuthHandler
         return result.toString();
     }
 
-    final boolean validString(String randomized)
+    final boolean validVerifier(String verifier)
     {
-        return randomized.length() >=  43 && randomized.length() <= 128;
+        return verifier.length() >=  43 && verifier.length() <= 128;
     }
 
-    final String sha256(String randomized)
+    final byte[] sha256(String randomized)
     {
         try
         {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] hash = digest.digest(randomized.getBytes(StandardCharsets.UTF_8));
-
-            StringBuilder result = new StringBuilder();
-            for(byte b : hash)
-            {
-                result.append(String.format("%02x", b));
-            }
-
-            return result.toString();
+            return digest.digest(randomized.getBytes(StandardCharsets.UTF_8));
         }
         catch (NoSuchAlgorithmException e)
         {
             throw new RuntimeException(e);
         }
+    }
+
+    final String base64Encoder(byte[] hashed)
+    {
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(hashed);
+    }
+
+    final String codeChallenge(String verifier)
+    {
+
+        if(!validVerifier(verifier))
+        {
+            throw new IllegalArgumentException("Invalid length.");
+        }
+
+        byte[] hashed = sha256(verifier);
+        return base64Encoder(hashed);
     }
 }
